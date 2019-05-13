a792778279c38ca70a932b577ade334624910d1b
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-13 19:07:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return ("I can\'t hear you!")
   } else if (string === string.toUpperCase()) {
-    return console.log("YES INDEED!")
+    return ("YES INDEED!")
   } else if (string === "I love you, Grandma.") {
-    return console.log("I love you, too.")
+    return ("I love you, too.")
   }
 }
